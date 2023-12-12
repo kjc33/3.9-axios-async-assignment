8c36d3a07460293d6a278af6ab17deea2edc5427
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2023-12-12 00:14:08 -0800

    Added API Info

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import DataService from "./api/DataService";
 
-export default function App() {
-  return <div>App</div>;
+function App() {
+  const [apiData, setApiData] = useState(null);
+
+  function apiGet() {
+    DataService.get("datasets")
+      .then((response) => {
+        console.log('Raw API Response:', response);
+        setApiData(response.data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }
+  return (
+    <div>
+      <h1>Data Service</h1>
+      <button onClick={apiGet}>Load Data</button>
+
+      {apiData && (
+        <div>
+          <h2>Data from NOAA API:</h2>
+          <pre>{JSON.stringify(apiData, null, 2)}</pre>
+        </div>
+      )}
+    </div>
+  );
 }
+
+export default App;
---
+++ src/api/DataService.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const BASE_URL = "https://www.ncdc.noaa.gov/cdo-web/api/v2/datasets";
+const API_KEY = "uGTpgMzYzfezYoTaZkwFgKnsyXeholXD";
+const DataService = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    token: API_KEY,
+  },
+});
+
+export default DataService;
